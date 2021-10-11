0a1ead36491eb042ce4d9305a677a83e3883fa24
KaMazim <kainamazim@gmail.com>
2021-10-11 10:53:39 -0300

    extendendo schemas

---
+++ src/ExtendendoSchemas/script.js
@@ -0,0 +1,61 @@
+const SimpleSchema = require("simpl-schema");
+
+const person_schema = new SimpleSchema({
+    id: {
+        type: SimpleSchema.oneOf(
+            SimpleSchema.Integer,
+            String
+        ),
+    },
+    name: {
+        type: String,
+        regEx: /^[a-zA-Z\sà-úÀ-Ú]+$/ //Expressão Regular que aceita letras, acentos e espaços
+    },
+
+    /*
+        other: { type: String, regEx: () => [/^[a-zA-Z]+$/, /[0-9]/] }
+    */
+
+    email: {
+        type: String,
+        regEx: SimpleSchema.RegEx.EmailWithTLD
+    }
+});
+
+const student_schema = new SimpleSchema({
+    name: {
+        type: String,
+        min: 16
+    },
+    grade: SimpleSchema.oneOf(
+        {
+            type: SimpleSchema.Integer,
+            min: 0,
+            max: 10
+        },
+        {
+            type: String,
+            allowedValues: ["A", "B", "C", "D", "E", "F"]
+        }
+    )
+});
+
+const test_object = {
+    id: "identificação",
+    name: "João Vitor 123456789", 
+    email: "name@email.com",
+    grade: "F"
+};
+
+
+/*   Validando   */
+
+const context = student_schema.newContext();
+
+context.validate(test_object);
+console.log(context.isValid(), context.validationErrors());
+
+student_schema.extend(person_schema);
+
+context.validate(test_object);
+console.log(context.isValid(), context.validationErrors());
